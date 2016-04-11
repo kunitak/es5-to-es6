7ebaf30fc34a8c5a03cf12d9c8ff359a0cbf5f9e
國田健史 <kunitak@solutionware.jp>
2016-04-12 08:52:53 +0900

    リファクタ

--- client/scripts/stores/storeUtils.js
+++ client/scripts/stores/storeUtils.js
@@ -2,17 +2,18 @@ import React from 'react';
 import request from 'superagent';
 
 let ajax = {
-  get  : (url, params, callback) => 
+  get  : (url, params, callback) => {
     request
       .get(url)
       .query(params)
       .end((err, res) => callback(err, res))
-  ,
-  post : (url, params, callback) => 
+  },
+  post : (url, params, callback) => {
     request
       .post(url)
       .send(params)
       .end((err, res) => callback(err, res))
+  }
 };
 
 export default ajax;
